cb01a6a0468806f5e434fea57a5ebf2cfb5be06b
Masaru Tsuchiyama <m.tmatma@gmail.com>
2024-12-14 07:53:09 +0900

    sample code

---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a = 0;
+    for( int i = 0; i < 10; i++ )
+    {
+        printf("Hello, World!\n");
+    }
+
+    if ( a == 0) {
+        printf("Hello, World!\n");
+    }
+    else if ( a == 1) {
+        printf("Hello, World!\n");
+    }
+    else {
+        printf("Hello, World!\n");
+    }
+
+    do {
+        printf("Hello, World!\n");
+    } while ( a != 0 );
+    return 0;
+}
